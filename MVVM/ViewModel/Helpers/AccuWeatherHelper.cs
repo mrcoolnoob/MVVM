722baa3e8de991c329b60d0762f71a9f92f1712a
mrcool <mrcool@gmail.com>
2023-09-24 15:26:24 +0500

    Add project files.

---
+++ MVVM/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,47 @@
+﻿using MVVM.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string API_KEY = "bbxA485t2XDviIGdje0K33vspE7xSAQS";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            var cities = new List<City>();
+            string url = BASE_URL + String.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+
+            }
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string cityKey)
+        {
+            var conditions = new CurrentConditions();
+            string url = BASE_URL + String.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                conditions = (JsonConvert.DeserializeObject<List<CurrentConditions>>(json)).FirstOrDefault();
+            }
+            return conditions;
+        }
+    }
+}
---
+++ MVVM/ViewModel/WeatherVM.cs
@@ -0,0 +1,101 @@
+﻿using MVVM.Model;
+using MVVM.ViewModel.Commands;
+using MVVM.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.ViewModel
+{
+    public class WeatherVM: INotifyPropertyChanged
+    {
+        #region CommandProperties
+
+        public SearchCommand SearchCommand { get; set; }
+        #endregion
+
+        #region AutoProperties
+        public ObservableCollection<City> Cities { get; set; }
+        #endregion
+
+        #region FullProperties
+
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set { query = value; OnPropertyChanged("Query"); }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set { 
+                currentConditions = value;  
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            {
+                Debug.WriteLine("Here 55");
+                Debug.WriteLine(value);
+                if(selectedCity != value)
+                {
+                    selectedCity = value;
+                    OnPropertyChanged("SelectedCity");
+                    Debug.WriteLine(value.LocalizedName);
+                    Debug.WriteLine(value.Key);
+                    GetCurrentConditions();
+                }
+            }
+        }
+
+        #endregion
+
+        #region EventProperties
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        public WeatherVM()
+        {
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
+        public async void GetCities()
+        {
+            var tempCities = await AccuWeatherHelper.GetCities(Query);
+            Cities.Clear();
+            foreach (City city in tempCities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        public async void GetCurrentConditions()
+        {
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));  
+        }
+
+    }
+}
